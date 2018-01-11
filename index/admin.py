755acafa45c15937f434bb144038be50cbfac838
Lython7 <15210370876@163.com>
2018-01-11 15:18:13 +0800

    test

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import *
 
 # Register your models here.
+
+# userprofile
+# class AccountInline(admin.StackedInline):
+#     model = Account
+#     can_delete = False
+#     verbose_name_plural = 'account'
+
+# class UserAdmin(BaseUserAdmin):
+#     inlines = (AccountInline,)
+
+# admin.site.unregister(User)
+# admin.site.register(User, UserAdmin)
+
+
+class News_center_Admin(admin.ModelAdmin):
+    list_display = ('news_title', 'news_time', 'created_time', 'created_by', )
+    search_fields = ('news_title', 'news_time', 'created_time', 'created_by', )
+    ordering = ('created_time',)
+    list_per_page = 20
+
+
+class Recruitment_info_Admin(admin.ModelAdmin):
+    list_display = ('working_area', 'position_name', 'created_time', 'created_by',)
+    search_fields = ('working_area', 'position_name', 'created_time', 'created_by',)
+    ordering = ('created_time',)
+    list_per_page = 20
+
+
+admin.site.register(News_center, News_center_Admin)
+admin.site.register(Recruitment_info, Recruitment_info_Admin)
--- index/models.py
+++ index/models.py
@@ -1,9 +1,54 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+#profile
+class uprofile():
+    pass
+
+
+# 新闻中心
 class News_center(models.Model):
-    news_title = models.CharField(max_length=50)
-    news_time = models.CharField(max_length=30)
-    news_image = models.ImageField()# 路径
-    news_content = models.CharField(max_length=3072)
-    created_time = models.DateTimeField(auto_now_add=True, editable=False)
+    choices = (
+        (False, '保存'),
+        (True, '删除'),
+    )
+    news_title = models.CharField(max_length=50, verbose_name='新闻标题', null=False)
+    news_time = models.CharField(max_length=30, verbose_name='新闻时间', null=False)
+    news_image = models.ImageField(verbose_name='新闻照片', null=False)# 路径
+    news_content = models.TextField(max_length=3072, verbose_name='新闻内容', null=False)
+    created_time = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='编辑时间', null=False)
+    created_by = models.ForeignKey(User, verbose_name='编辑人', null=False)# 编辑人 editable=False default =当前登录的id
+    is_delete = models.BooleanField(choices=choices, default=False, null=False, verbose_name='逻辑删除')
+
+    class Meta:
+        verbose_name = '新闻中心'
+        verbose_name_plural = '新闻中心'
+
+    def __str__(self):
+        return self.news_title
+
+
+# 招聘信息
+class Recruitment_info(models.Model):
+    choices = (
+        (False, '保存'),
+        (True, '删除'),
+    )
+    working_area = models.CharField(max_length=20, verbose_name='工作地区', null=False)
+    position_name = models.CharField(max_length=20, verbose_name='工作岗位', null=False)
+    number = models.IntegerField(verbose_name='招聘人数', null=False)
+    experience = models.CharField(max_length=20, verbose_name='工作经验')# 工作经验 可以作为选择模式
+    necessary_skills = models.TextField(max_length=1024, verbose_name='技能要求', null=False)# 能力需求
+    job_responsibilities = models.TextField(max_length=1024, verbose_name='岗位职责', null=False)
+    position_salary = models.CharField(max_length=5, verbose_name='薪资待遇')# 薪资待遇
+    created_time = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='编辑时间', null=False)
+    created_by = models.ForeignKey(User, verbose_name='编辑人', null=False)# 编辑人 editable=False default =
+    # created_by = models.ForeignKey(User, verbose_name='编辑人', null=False)# 编辑人 editable=False
+    is_delete = models.BooleanField(choices=choices, default=False, null=False, verbose_name='逻辑删除')# 逻辑删除
+
+    class Meta:
+        verbose_name = '招聘信息'
+        verbose_name_plural = '招聘信息'
+
+    def __str__(self):
+        return self.position_name
