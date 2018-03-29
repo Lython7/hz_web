d2d3cd89032e1efbeb50cbe0ba8e699614d6bc98
zhengwei2531 <1820468354@qq.com>
2018-03-29 15:35:42 +0800

    默认的

--- hz_web/settings.py
+++ hz_web/settings.py
@@ -148,6 +148,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static') # 部署时需要使用
