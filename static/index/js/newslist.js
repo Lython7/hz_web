28b620e2241d857d15bb589817f6cb663699dfd5
zhengwei2531 <1820468354@qq.com>
2018-03-30 16:51:15 +0800

    默认的

--- static/index/js/newslist.js
+++ static/index/js/newslist.js
@@ -2,11 +2,16 @@ function htadd(data) {
     var ht='';
     ht+=' <div class="first">';
 	ht+='<div class="news_content"><div class="img_left">';
-    var imgUrl = data.news_image1.split('/');
-    imgUrl.shift();
-    imgUrl.shift();
-    var ImgUrls = imgUrl.join('/');
-	ht+='<img src="/static/' + ImgUrls + '"/></div><div class="con_right">';
+	if (data.news_image1.indexOf('http')===-1){
+	    var imgUrl = data.news_image1.split('/');
+        imgUrl.shift();
+        imgUrl.shift();
+        var ImgUrls = imgUrl.join('/');
+        ht+='<img src="/static/' + ImgUrls + '"/></div><div class="con_right">';
+    }else {
+	    ht+='<img src="' + data.news_image1 + '"/></div><div class="con_right">';
+    }
+
 	ht+='<a class="tit" href="newsDetails.html">' + data.news_title + '</a>';
 	ht+='<div class="con"><a href="newsDetails.html">' + data.news_content + '</a></div></div></div></div>';
 	return ht;
