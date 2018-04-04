68c582fe53cc392c5c1596e1b53bf4b0887eb4ff
zhengwei2531 <1820468354@qq.com>
2018-04-04 11:28:41 +0800

    默认的

---
+++ static/index/js/newsDetail.js
@@ -0,0 +1,25 @@
+$(function () {
+    navIndex(1);
+    /*$.ajax({
+        url: '../api/newslist/',
+        type: 'GET',
+        success: function(data) {
+            console.log(data);
+            callback(data);
+        },
+        error: function(xhr, textStatus) {
+            alert('加载失败，请稍后再试！');
+            console.log(xhr);
+            console.log(textStatus);
+        }
+    });*/
+});
+/*
+function callback(data) {
+    var oNews = document.getElementsByClassName('news_')[0];
+    var newsht='';
+    for(var item in data.results){
+        newsht += htadd(data.results[item]);
+    }
+    oNews.innerHTML=newsht;
+}*/
