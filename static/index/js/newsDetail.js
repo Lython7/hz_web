053007c30e27465e6dfefe25daec1064388f2586
zhengwei2531 <1820468354@qq.com>
2018-04-04 14:16:51 +0800

    默认的

--- static/index/js/newsDetail.js
+++ static/index/js/newsDetail.js
@@ -14,12 +14,20 @@ $(function () {
         }
     });
 });
-/*
 function callback(data) {
-    var oNews = document.getElementsByClassName('news_')[0];
-    var newsht='';
-    for(var item in data.results){
-        newsht += htadd(data.results[item]);
+    var oNews = document.getElementsByClassName('details')[0];
+    var time = data.news_time.split('T')[0];
+    var newsht = '';
+    newsht += '<h1 class="title">' + data.news_title + '</h1>';
+    newsht += '<div class="time">' + time + '</div>';
+    for (var i=1; i<=20; i++) {
+        if (data['news_image'+i]){
+            newsht+='<div class="img"><img src="' + data['news_image'+i] + '"/></div>';
+        }
+        if (data['news_content'+i]){
+            newsht+='<p>' + data['news_content'+i] + '</p>';
+            console.log(1)
+        }
     }
     oNews.innerHTML=newsht;
-}*/
+}
